7ed9bbb15aebd5919c2487606d8d16b53cdecc75
Puputpuspita <71245135+Puputpuspita@users.noreply.github.com>
2020-09-17 21:32:26 +0700

    Puput.java

---
+++ Puput.java
@@ -0,0 +1,7 @@
+public class Puput{
+	public static void main(String[] args){
+	System.out.println("Nama	: Puput Puspita");
+	System.out.println("NIM	: 6706202132");
+	System.out.println("Kelas	: D3IF-44-03");
+	}
+}
